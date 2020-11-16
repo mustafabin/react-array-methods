506e06bb7f970c1ffa729eb6914b21a36d042667
soleilyasmina <mounlasolomon@gmail.com>
2020-11-16 11:07:26 -0500

    remove solution code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import { useState } from "react";
 import flavors from "./flavors.json";
-import Filter from "./components/Filter";
-import Find from "./components/Find";
 import Map from "./components/Map";
 import Nav from "./components/Nav";
-import Reduce from "./components/Reduce";
-import Some from "./components/Some";
 import "./App.css";
 
 function App() {
@@ -14,18 +10,6 @@ function App() {
   let content;
 
   switch (current) {
-    case 'filter':
-      content = <Filter flavors={flavors} />;
-      break;
-    case 'find':
-      content = <Find flavors={flavors} />;
-      break;
-    case 'reduce':
-      content = <Reduce flavors={flavors} />;
-      break;
-    case 'some':
-      content = <Some flavors={flavors} />;
-      break;
     case 'map':
       content = <Map flavors={flavors} />;
       break;
